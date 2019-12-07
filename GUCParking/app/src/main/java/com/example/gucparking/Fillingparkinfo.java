f0e5dfbf226d4bd5dab5410df1ddab4f395d5e72
decapood <omar.abdelkarim@student.guc.edu.eg>
2019-12-07 22:41:20 +0200

    connecting filling update activity to node.js server

--- GUCParking/app/src/main/java/com/example/gucparking/Fillingparkinfo.java
+++ GUCParking/app/src/main/java/com/example/gucparking/Fillingparkinfo.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.textclassifier.TextLinks;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -13,6 +14,13 @@ import android.widget.Spinner;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class Fillingparkinfo extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
@@ -24,9 +32,11 @@ public class Fillingparkinfo extends AppCompatActivity implements AdapterView.On
     private static final String[] paths = {"Gate1", "Gate2", "Gate3","Gate4","Gate5","OnCampusParking"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final String URL = "http://localhost:8080/addingUpdate";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fillingparkinfo);
         Button done = findViewById(R.id.button2);
+
 
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
@@ -44,10 +54,35 @@ public class Fillingparkinfo extends AppCompatActivity implements AdapterView.On
         done.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                String userinfo = username +" parked "+ zone;
-//                Intent i = new Intent(Status.this,Updates.class);
-//                i.putExtra("user",userinfo);
-//                startActivity(i);
+                String userinfo = username +"has parked at "+ zone;
+                OkHttpClient client = new OkHttpClient();
+
+                RequestBody formbody = new FormEncodingBuilder()
+                        .add("update",userinfo)
+                        .build();
+                Request request = new Request.Builder()
+                        .url(URL)
+                        .post(formbody)
+                        .build();
+                try{
+                    Response response = client.newCall(request).execute();
+                    if (!response.isSuccessful()) System.out.println("cannot insert");
+                    else {
+                        System.out.println("Successful");
+                        Intent i = new Intent(Fillingparkinfo.this,Updates.class);
+                        startActivity(i);
+                    }
+
+                    try{
+                        System.out.println(response.body().string());
+                    }
+                    catch(Exception e){}
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+
+
 
             }
         });
